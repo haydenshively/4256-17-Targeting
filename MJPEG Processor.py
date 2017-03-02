addf470c56976448ca8419bc96bc01cf0b6309b3
WCAFRC42562 <cyborgcats4256@wcastl.org>
2017-03-01 21:50:58 -0600

    Added NetworkTables

--- MJPEG Processor.py
+++ MJPEG Processor.py
@@ -1,15 +1,19 @@
 import cv2
 import numpy as np
+from networktables import NetworkTables
 import urllib
 import time
 
-url = 'http://192.168.0.146:4747/mjpegfeed?340x280'#'http://10.42.56.3/mjpg/video.mjpg'
-stream = urllib.urlopen(url)
+cameraURL = 'http://10.42.56.3/mjpg/video.mjpg'#'http://192.168.0.146:4747/mjpegfeed?340x280'
+stream = urllib.urlopen(cameraURL)
+rioURL = 'roborio-4256-frc.local'
+NetworkTables.initialize(server = rioURL)
+table = NetworkTables.getTable('axis')
 bytes = ''
 image_bytes = ''
 found = False
-start = int(time.time()*1000)
 frames = 0
+start = int(time.time()*1000)
 
 while (True):
     bytes = stream.read(1024)
@@ -22,11 +26,6 @@ while (True):
             found = True
         else:
             image_bytes += bytes[a:]
-    elif (a is not -1 and image_bytes is not ''):#hit an image header and need to complete old jpg
-        b = bytes.find('\xff\xd9')
-        jpg = image_bytes + bytes[:b+2]
-        image_bytes = bytes[a:]
-        found = True
     elif (a is -1 and image_bytes is not ''):#working through an intermediate section
         b = bytes.find('\xff\xd9')
         if (b is not -1):
@@ -35,10 +34,23 @@ while (True):
             found = True
         else:
             image_bytes += bytes
+    elif (a is not -1 and image_bytes is not ''):#hit an image header and need to complete old jpg
+        b = bytes.find('\xff\xd9')
+        jpg = image_bytes + bytes[:b+2]
+        image_bytes = bytes[a:]
+        found = True
     if (found):
         frames += 1
         print(frames*1000/(int(time.time()*1000) - start))
         frame = cv2.imdecode(np.fromstring(jpg, dtype = np.uint8), cv2.IMREAD_COLOR)
+        #-----------------------------------------------------------------------
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        v = hsv[:,:,2]
+        v[v >= 140] = 255
+        v[v < 140] = 0
+        hsv[:,:,2] = v
+        frame = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        #-----------------------------------------------------------------------
         cv2.imshow("frame", frame)
         found = False
         ch = 0xFF & cv2.waitKey(1)
